1b0ffb86292c9493b3b7f774fb472e0f4045bbd4
薄炳鑫 <bobingxin@bobingxindeMacBook-Pro.local>
2022-07-03 23:12:20 +0800

    添加账户信息

--- account.js
+++ account.js
@@ -3,6 +3,14 @@ class Account extends Okx {
   async config() {
     return await this.http('get', '/api/v5/account/config', {});
   }
+
+  async setLeverage(data = {}) {
+    return await this.http('post', '/api/v5/account/set-leverage', data);
+  }
+
+  async setLeverage() {
+    return await this.http('get', '/api/v5/account/leverage-info', {});
+  }
 }
 
 module.exports = Account;
