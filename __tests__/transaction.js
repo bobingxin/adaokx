1e4a202795b45b0d9bb9c9a1f58bdadc8f1c752d
bobingxin <bobingxin@qq.com>
2022-10-24 22:03:14 +0800

    添加单元测试

--- __tests__/transaction.js
+++ __tests__/transaction.js
@@ -9,7 +9,7 @@ test("test_order", async () => {
     okxData.passphrase
   );
   const cusOrderNumber = moment().valueOf();
-
+  console.log(cusOrderNumber);
   const orderInfo = await transaction.order({
     instId: "ETH-USDT-SWAP", // 产品ID
     tdMode: "cross", // 保证金模式：isolated：逐仓 ；cross：全仓
@@ -35,4 +35,47 @@ test("order_info", async () => {
     clOrdId: "1657331653792",
   });
   console.log(orderInfo);
+});
+
+test("order_algo", async () => {
+  const transaction = new Transcation(
+    okxData.apikey,
+    okxData.secretkey,
+    okxData.passphrase
+  );
+
+  const algoRes = await transaction.orderAlgo({
+    instId: "ETH-USDT-SWAP", // 产品ID
+    tdMode: "cross", // 保证金模式：isolated：逐仓 ；cross：全仓
+    side: "sell", // 订单方向 buy：买 sell：卖
+    posSide: "long", // 持仓方向 在双向持仓模式下必填，且仅可选择 long 或 short
+    ordType: "oco", // oco：双向止盈止损
+    sz: 10, // 委托数量
+    tag: "0c4bc633283dBCDE",
+    tpTriggerPx: "1400", // 止盈触发价
+    tpTriggerPxType: "last", // 止盈触发价类型 last：最新价格
+    tpOrdPx: "-1", // 止盈触发价, 委托价格为-1时，执行市价止盈
+    slTriggerPx: "1300", // 止损触发价
+    slTriggerPxType: "last", // 止损触发价类型 last：最新价格
+    slOrdPx: "-1", // 止损触发价, 委托价格为-1时，执行市价止损
+  });
+  console.log(algoRes);
+});
+//504774100720680960
+test("order_cancel_algo", async () => {
+  const transaction = new Transcation(
+    okxData.apikey,
+    okxData.secretkey,
+    okxData.passphrase
+  );
+  try {
+    const cancelRes = await transaction.cancelAlgos([
+      {
+        instId: "ETH-USDT-SWAP", // 产品ID
+        algoId: "504774100720680960", // 保证金模式：isolated：逐仓 ；cross：全仓
+      },
+    ]);
+  } catch (e) {
+    console.log(e);
+  }
 });
